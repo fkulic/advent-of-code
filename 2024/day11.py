31c93b57c8279584285300a9b2893926967590a1
Filip Kulic <filip.kulic@outlook.com>
2024-12-11 13:32:20 +0100

    Use functools cache

--- 2024/day11.py
+++ 2024/day11.py
@@ -1,7 +1,7 @@
-from collections import defaultdict
-from pathlib import Path
-from math import log10
 import sys
+from functools import cache
+from math import log10
+from pathlib import Path
 
 
 def split(s: int) -> tuple[int | int] | None:
@@ -14,24 +14,16 @@ def split(s: int) -> tuple[int | int] | None:
     return False
 
 
-CACHE = defaultdict(dict)
-
-
-def blink_n(s: int, n: int, c: int = 0) -> int:
-    global CACHE
+@cache
+def blink_n(s: int, n: int) -> int:
     if n == 0:
         return 1
-
-    if x := CACHE.get(s, {}).get(c):
-        return x
-
     if s == 0:
-        ret = blink_n(1, n - 1, c + 1)
+        ret = blink_n(1, n - 1)
     elif splitted_s := split(s):
-        ret = blink_n(splitted_s[0], n - 1, c + 1) + blink_n(splitted_s[1], n - 1, c + 1)
+        ret = blink_n(splitted_s[0], n - 1) + blink_n(splitted_s[1], n - 1)
     else:
-        ret = blink_n(2024 * s, n - 1, c + 1)
-    CACHE[s][c] = ret
+        ret = blink_n(2024 * s, n - 1)
     return ret
 
 
@@ -41,7 +33,5 @@ if __name__ == "__main__":
 
     stones = [int(x) for x in input_path.read_text().strip().split()]
 
-    CACHE.clear()
     print("FIRST PART", sum(blink_n(s, 25) for s in stones))
-    CACHE.clear()
     print("SECOND PART", sum(blink_n(s, 75) for s in stones))
