7e42ebdc4a659b058e55fb8bd154ba89f76bda8d
Filip Kulic <filip.kulic@outlook.com>
2025-01-18 11:13:02 +0100

    Clean up 2019 day 18

--- 2019/day18.py
+++ 2019/day18.py
@@ -16,65 +16,72 @@ class Point:
 
 DIRECTIONS = [Point(-1, 0), Point(0, 1), Point(1, 0), Point(0, -1)]
 
-ALL_KEYS = 0
-KEYS: dict[int, Point] = {}
-KEY_POSES: dict[Point, int] = {}
-DOORS: dict[int, Point] = {}
-DOOR_POSES: dict[Point, int] = {}
 
-PATH_CACHE = {}
+class PathsCalc:
+    def __init__(self, walls: set[Point], key_poses: dict[Point, int], door_poses: dict[Point, int]):
+        self._path_cache = {}
+        self._walls = walls
+        self._key_poses = key_poses
+        self._door_poses = door_poses
 
+    def get_paths(self, start: Point) -> dict[int, dict[int, int]]:
+        if start in self._path_cache:
+            return self._path_cache[start]
 
-def get_paths(start, walls):
-    if start in PATH_CACHE:
-        return PATH_CACHE[start]
+        Q = deque([(start, 0, 0)])
+        paths = defaultdict(dict)
+        visited = defaultdict(set)
 
-    Q = deque([(start, 0, 0)])
-    paths = defaultdict(dict)
-    visited = defaultdict(set)
+        while Q:
+            p, cost, doors_passed = Q.popleft()
 
-    while Q:
-        p, cost, doors_passed = Q.popleft()
+            already_visited = False
+            for prev_doors_passed in visited[p]:
+                if (prev_doors_passed & doors_passed) == prev_doors_passed:
+                    already_visited = True
+                    break
 
-        already_visited = False
-        for prev_doors_passed in visited[p]:
-            if (prev_doors_passed & doors_passed) == prev_doors_passed:
-                already_visited = True
-                break
-
-        if already_visited:
-            continue
-
-        visited[p].add(doors_passed)
-
-        for d in DIRECTIONS:
-            new_p = p + d
-            if new_p in walls:
+            if already_visited:
                 continue
 
-            new_doors_passed = doors_passed
-            if new_p in DOOR_POSES:
-                new_doors_passed |= DOOR_POSES[new_p]
+            visited[p].add(doors_passed)
 
-            if new_p in KEY_POSES:
-                key_id = KEY_POSES[new_p]
-                if new_doors_passed not in paths[key_id]:
-                    paths[key_id][new_doors_passed] = cost + 1
+            for d in DIRECTIONS:
+                new_p = p + d
+                if new_p in self._walls:
+                    continue
 
-            Q.append((new_p, cost + 1, new_doors_passed))
-    PATH_CACHE[start] = paths
-    return paths
+                new_doors_passed = doors_passed
+                if new_p in self._door_poses:
+                    new_doors_passed |= self._door_poses[new_p]
+
+                if new_p in self._key_poses:
+                    key_id = self._key_poses[new_p]
+                    if new_doors_passed not in paths[key_id]:
+                        paths[key_id][new_doors_passed] = cost + 1
+
+                Q.append((new_p, cost + 1, new_doors_passed))
+        self._path_cache[start] = paths
+        return paths
 
 
-def part_one(start, walls) -> int:
-    visited = {}
-    Q = deque([(start, 0, 0)])
+def part_one(
+    start: Point,
+    walls: set[Point],
+    keys: dict[int, Point],
+    key_poses: dict[Point, int],
+    door_poses: dict[Point, int],
+) -> int:
+    path_calc = PathsCalc(walls, key_poses, door_poses)
+    all_keys = sum(keys)
+
     min_cost = sys.maxsize
-
+    Q = deque([(start, 0, 0)])
+    visited = {}
     while Q:
         current_pos, current_cost, collected_keys = Q.popleft()
 
-        if collected_keys == ALL_KEYS:
+        if collected_keys == all_keys:
             min_cost = min(current_cost, min_cost)
             continue
 
@@ -85,7 +92,7 @@ def part_one(start, walls) -> int:
             continue
         visited[(current_pos, collected_keys)] = current_cost
 
-        paths = get_paths(current_pos, walls)
+        paths = path_calc.get_paths(current_pos)
         for key, paths_doors in paths.items():
             if key & collected_keys != 0:
                 continue
@@ -93,7 +100,7 @@ def part_one(start, walls) -> int:
             for doors, cost in paths_doors.items():
                 if (collected_keys & doors) == doors:
                     new_collected = collected_keys | key
-                    Q.append((KEYS[key], current_cost + cost, new_collected))
+                    Q.append((keys[key], current_cost + cost, new_collected))
 
     return min_cost
 
@@ -109,19 +116,25 @@ def transform(portals, walls):
     return portals, walls
 
 
-def part_two(portals: list[Point], walls: set[Point]) -> int:
-    PATH_CACHE.clear()
+def part_two(
+    portals: list[Point],
+    walls: set[Point],
+    keys: dict[int, Point],
+    key_poses: dict[Point, int],
+    door_poses: dict[Point, int],
+) -> int:
     if len(portals) == 1:
         portals, walls = transform(portals, walls)
 
-    visited = {}
+    path_calc = PathsCalc(walls, key_poses, door_poses)
+    all_keys = sum(keys)
+
     min_cost = sys.maxsize
-
     Q = deque([(portals, [0, 0, 0, 0], 0)])
-
+    visited = {}
     while Q:
         current_positions, current_costs, collected_keys = Q.popleft()
-        possible_combinations = [[], [], [], []]
+        possible_combinations = [[(pos, 0)] for pos in current_positions]
 
         total_cost = sum(current_costs)
         if total_cost > min_cost:
@@ -132,23 +145,20 @@ def part_two(portals: list[Point], walls: set[Point]) -> int:
             continue
         visited[visited_key] = total_cost
 
+        if collected_keys == all_keys:
+            min_cost = min(min_cost, total_cost)
+            continue
+
         for i in range(4):
             current_pos = current_positions[i]
-            paths = get_paths(current_pos, walls)
+            paths = path_calc.get_paths(current_pos)
             for key, paths_doors in paths.items():
                 if key & collected_keys != 0:
                     continue
 
                 for doors, cost in paths_doors.items():
                     if (collected_keys & doors) == doors:
-                        possible_combinations[i].append((KEYS[key], cost))
-
-        if collected_keys == ALL_KEYS:
-            min_cost = min(min_cost, sum(current_costs))
-            continue
-
-        for i in range(4):
-            possible_combinations[i].append((current_positions[i], 0))
+                        possible_combinations[i].append((keys[key], cost))
 
         for quadrant_combinations in product(*possible_combinations):
             new_positions = []
@@ -157,7 +167,7 @@ def part_two(portals: list[Point], walls: set[Point]) -> int:
 
             for i, (pos, cost) in enumerate(quadrant_combinations):
                 new_positions.append(pos)
-                new_collected_keys |= KEY_POSES.get(pos, 0)
+                new_collected_keys |= key_poses.get(pos, 0)
                 new_costs.append(current_costs[i] + cost)
 
             if not all(current_positions[i] == new_positions[i] for i in range(4)):
@@ -188,23 +198,21 @@ if __name__ == "__main__":
     mappings = Mappings()
     walls = set()
     portals = []
+    keys, key_poses, door_poses = {}, {}, {}
 
     bit_keys = {}
-    ALL_KEYS = 0
     for i, line in enumerate(data.splitlines()):
         for j, c in enumerate(line):
             p = Point(i, j)
 
             if c.islower():
                 bit_key = mappings.as_bit(c)
-                KEYS[bit_key] = p
-                KEY_POSES[p] = bit_key
-                ALL_KEYS |= bit_key
+                keys[bit_key] = p
+                key_poses[p] = bit_key
 
             elif c.isupper():
                 bit_key = mappings.as_bit(c)
-                DOORS[bit_key] = p
-                DOOR_POSES[p] = bit_key
+                door_poses[p] = bit_key
 
             elif c == "#":
                 walls.add(p)
@@ -212,5 +220,5 @@ if __name__ == "__main__":
             elif c == "@":
                 portals.append(p)
 
-    print("FIRST PART", part_one(portals[0], walls))
-    print("SECOND PART", part_two(portals, walls))
+    print("FIRST PART", part_one(portals[0], walls, keys, key_poses, door_poses))
+    print("SECOND PART", part_two(portals, walls, keys, key_poses, door_poses))
